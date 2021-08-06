b4abcd14117088b8b57404392db2c2b886eb0a1a
SuchetanaSheet <2018suchetana@gmail.com>
2021-08-06 21:43:03 +0530

    Weather done

---
+++ js/config.js
@@ -0,0 +1,6 @@
+/*var config = {
+    MY_API_TOKEN: '2e98326a69eb2574d75056c5082442c8',
+    CITY_TOKEN: 'dab3af44de7d24ae7ff86549334e45bd',
+}*/
+2e98326a69eb2574d75056c5082442c8 //key
+api.openweathermap.org / data / 2.5 / weather ? q = { city name } & appid={ API key }
